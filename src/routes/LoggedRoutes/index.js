c68c6d68c0f1059146f9d17b27d30e412e65cc64
matheusma37 <mtsma37@gmail.com>
2021-05-07 00:09:35 -0300

    Implement the initial routes for the logged in user

---
+++ src/routes/LoggedRoutes/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import HomeScreen from '../../screens/HomeScreen';
+import { lightGray, lightOrange } from '../../assets/colors';
+
+const Tab = createBottomTabNavigator();
+
+const LoggedRoutes = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName='Home'
+      tabBarOptions={{
+        style: {
+          height: 60,
+          backgroundColor: lightGray,
+          borderTopColor: 'transparent'
+        },
+        activeTintColor: lightOrange,
+        inactiveTintColor: 'white',
+        tabStyle: {
+          paddingBottom: 5,
+          paddingTop: 5
+        }
+      }}>
+      <Tab.Screen
+        name="Perfil"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Icon name='person' color={color} size={size} />
+          )
+        }} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Icon name='home' color={color} size={size} />
+          )
+        }} />
+      <Tab.Screen
+        name="Consultas"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Icon name='date-range' color={color} size={size} />
+          )
+        }} />
+    </Tab.Navigator>
+  );
+}
+
+export default LoggedRoutes;
